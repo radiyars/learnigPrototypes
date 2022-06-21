c0cac988cc6b123f298d34837fef94e85a978268
Radiy <radiyars@yandex.ru>
2022-06-21 12:56:48 +0300

    learning Protoypes

---
+++ script.js
@@ -0,0 +1,76 @@
+//? Задача состоит из двух частей.
+
+// У нас есть объекты:
+
+// let head = {
+// 	glasses: 1
+// };
+
+// let table = {
+// 	pen: 3
+// };
+
+// let bed = {
+// 	sheet: 1,
+// 	pillow: 2
+// };
+
+// let pockets = {
+// 	money: 2000
+// };
+
+// С помощью свойства __proto__ задайте прототипы так, чтобы поиск любого свойства выполнялся по следующему пути: pockets → bed → table → head.Например, pockets.pen должно возвращать значение 3(найденное в table), а bed.glasses – значение 1(найденное в head).
+
+let head = {
+	glasses: 1
+};
+
+let table = {
+	pen: 3,
+	__proto__: head
+
+};
+
+let bed = {
+	sheet: 1,
+	pillow: 2,
+	__proto__: table,
+
+};
+
+let pockets = {
+	money: 2000,
+	__proto__: bed
+};
+
+
+//? У нас есть два хомяка: шустрый (speedy) и ленивый (lazy); оба наследуют от общего объекта hamster.
+
+// Когда мы кормим одного хомяка, второй тоже наедается. Почему? Как это исправить?
+
+let hamster = {
+	stomach: [],
+
+	eat(food) {
+		this.stomach.push(food);
+	}
+};
+
+let speedy = {
+	stomach: [],
+
+	__proto__: hamster
+};
+
+let lazy = {
+	stomach: [],
+
+	__proto__: hamster
+};
+
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
+
+// У этого хомяка тоже есть еда. Почему? Исправьте
+console.log(lazy.stomach); // apple
